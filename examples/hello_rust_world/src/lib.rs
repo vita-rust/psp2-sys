2a27f971f04dad1ccf1fa31fdf820fdfeebe5f6c
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-05-24 13:39:33 +0200

    Add livearea assets to `hello_rust_world` example

--- examples/hello_rust_world/src/lib.rs
+++ examples/hello_rust_world/src/lib.rs
@@ -19,10 +19,7 @@ pub extern "C" fn panic_fmt() -> ! {
 #[no_mangle]
 pub unsafe fn main(_argc: isize, _argv: *const *const u8) -> isize {
     let mut screen = debug::screen::DebugScreen::new();
-    screen.puts(b"Hello, world!\n\0");
-    screen.puts(b"How are you?\n\0");
-    // screen.puts(b"\x1b[1;34mWow, this is blue !\x1b[0m]");
-
+    screen.puts(b"This bare-metal is starting to rust!\n\0");
     psp2::kernel::threadmgr::sceKernelDelayThread(3 * 1000000); // Wait for 3 seconds
     psp2::kernel::processmgr::sceKernelExitProcess(0);
     return 0;
