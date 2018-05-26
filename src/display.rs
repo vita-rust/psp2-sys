1adfd6551c476043e65d36da54776fe0088b7663
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-05-26 22:46:44 +0200

    Reformat `::display` and `::sysmem`

--- src/display.rs
+++ src/display.rs
@@ -1,48 +1,52 @@
-use ::types::SceSize;
-use ::types::SceUID;
+use types::SceSize;
+use types::SceUID;
 
 #[repr(u32)]
 pub enum SceDisplayErrorCode {
-    SCE_DISPLAY_ERROR_OK                    = 0,
-    SCE_DISPLAY_ERROR_INVALID_HEAD          = 0x80290000,
-    SCE_DISPLAY_ERROR_INVALID_VALUE         = 0x80290001,
-    SCE_DISPLAY_ERROR_INVALID_ADDR          = 0x80290002,
-    SCE_DISPLAY_ERROR_INVALID_PIXELFORMAT   = 0x80290003,
-    SCE_DISPLAY_ERROR_INVALID_PITCH         = 0x80290004,
-    SCE_DISPLAY_ERROR_INVALID_RESOLUTION    = 0x80290005,
-    SCE_DISPLAY_ERROR_INVALID_UPDATETIMING  = 0x80290006,
-    SCE_DISPLAY_ERROR_NO_FRAME_BUFFER       = 0x80290007,
-    SCE_DISPLAY_ERROR_NO_PIXEL_DATA         = 0x80290008,
-    SCE_DISPLAY_ERROR_NO_OUTPUT_SIGNAL      = 0x80290009
+    SCE_DISPLAY_ERROR_OK = 0,
+    SCE_DISPLAY_ERROR_INVALID_HEAD = 0x80290000,
+    SCE_DISPLAY_ERROR_INVALID_VALUE = 0x80290001,
+    SCE_DISPLAY_ERROR_INVALID_ADDR = 0x80290002,
+    SCE_DISPLAY_ERROR_INVALID_PIXELFORMAT = 0x80290003,
+    SCE_DISPLAY_ERROR_INVALID_PITCH = 0x80290004,
+    SCE_DISPLAY_ERROR_INVALID_RESOLUTION = 0x80290005,
+    SCE_DISPLAY_ERROR_INVALID_UPDATETIMING = 0x80290006,
+    SCE_DISPLAY_ERROR_NO_FRAME_BUFFER = 0x80290007,
+    SCE_DISPLAY_ERROR_NO_PIXEL_DATA = 0x80290008,
+    SCE_DISPLAY_ERROR_NO_OUTPUT_SIGNAL = 0x80290009,
 }
 
 #[repr(i32)]
 pub enum SceDisplayPixelFormat {
-    SCE_DISPLAY_PIXELFORMAT_A8B8G8R8        = 0x00000000,
+    SCE_DISPLAY_PIXELFORMAT_A8B8G8R8 = 0x00000000,
 }
 
 #[repr(i32)]
 pub enum SceDisplaySetBufSync {
-        /** Buffer change effective immediately */
-        SCE_DISPLAY_SETBUF_IMMEDIATE = 0,
-        /** Buffer change effective next frame */
-        SCE_DISPLAY_SETBUF_NEXTFRAME = 1
+    SCE_DISPLAY_SETBUF_IMMEDIATE = 0,
+    SCE_DISPLAY_SETBUF_NEXTFRAME = 1,
 }
 
 #[repr(C)]
 pub struct SceDisplayFrameBuf {
-        pub size: SceSize,      //  sizeof(SceDisplayFrameBuf)
-        pub base: *mut ::void,  //  Pointer to framebuffer
-        pub pitch: u32,         //  pitch pixels
-        pub pixelformat: u32,   //  pixel format (one of ::SceDisplayPixelFormat)
-        pub width: u32,         //  framebuffer width
-        pub height: u32         //  framebuffer height
+    pub size: SceSize,     //  sizeof(SceDisplayFrameBuf)
+    pub base: *mut ::void, //  Pointer to framebuffer
+    pub pitch: u32,        //  pitch pixels
+    pub pixelformat: u32,  //  pixel format (one of ::SceDisplayPixelFormat)
+    pub width: u32,        //  framebuffer width
+    pub height: u32,       //  framebuffer height
 }
 
 #[link(kind = "static", name = "SceDisplay_stub")]
 extern "C" {
-    pub fn sceDisplaySetFrameBuf(pParam: *const SceDisplayFrameBuf, sync: SceDisplaySetBufSync) -> i32;
-    pub fn sceDisplayGetFrameBuf(pParam: *mut SceDisplayFrameBuf, sync: SceDisplaySetBufSync) -> i32;
+    pub fn sceDisplaySetFrameBuf(
+        pParam: *const SceDisplayFrameBuf,
+        sync: SceDisplaySetBufSync,
+    ) -> i32;
+    pub fn sceDisplayGetFrameBuf(
+        pParam: *mut SceDisplayFrameBuf,
+        sync: SceDisplaySetBufSync,
+    ) -> i32;
     pub fn sceDisplayGetPrimaryHead() -> i32;
     pub fn sceDisplayGetRefreshRate(pFps: *mut f32) -> i32;
     pub fn sceDisplayGetMaximumFrameBufResolution(width: *mut i32, height: *mut i32) -> i32;
--- src/kernel/sysmem.rs
+++ src/kernel/sysmem.rs
@@ -1,76 +1,88 @@
-use ::types::SceUID;
-use ::types::SceSize;
-use ::types::SceUInt32;
+use types::SceSize;
+use types::SceUID;
+use types::SceUInt32;
 
 #[repr(i32)]
 pub enum SceKernelMemBlockType {
-    SCE_KERNEL_MEMBLOCK_TYPE_USER_RW_UNCACHE          = 0x0C208060,
-    SCE_KERNEL_MEMBLOCK_TYPE_USER_RW                  = 0x0C20D060,
-    SCE_KERNEL_MEMBLOCK_TYPE_USER_MAIN_PHYCONT_RW     = 0x0C80D060,
-    SCE_KERNEL_MEMBLOCK_TYPE_USER_MAIN_PHYCONT_NC_RW  = 0x0D808060,
-    SCE_KERNEL_MEMBLOCK_TYPE_USER_CDRAM_RW            = 0x09408060
+    SCE_KERNEL_MEMBLOCK_TYPE_USER_RW_UNCACHE = 0x0C208060,
+    SCE_KERNEL_MEMBLOCK_TYPE_USER_RW = 0x0C20D060,
+    SCE_KERNEL_MEMBLOCK_TYPE_USER_MAIN_PHYCONT_RW = 0x0C80D060,
+    SCE_KERNEL_MEMBLOCK_TYPE_USER_MAIN_PHYCONT_NC_RW = 0x0D808060,
+    SCE_KERNEL_MEMBLOCK_TYPE_USER_CDRAM_RW = 0x09408060,
 }
 
 #[repr(C)]
 pub struct SceKernelAllocMemBlockOpt {
-        pub size: SceSize,
-        pub attr: SceUInt32,
-        pub alignment: SceSize,
-        pub uidBaseBlock: SceUInt32,
-        pub strBaseBlockName: *const u8,
-        pub flags: i32,
-        pub reserved: [i32; 10],
+    pub size: SceSize,
+    pub attr: SceUInt32,
+    pub alignment: SceSize,
+    pub uidBaseBlock: SceUInt32,
+    pub strBaseBlockName: *const u8,
+    pub flags: i32,
+    pub reserved: [i32; 10],
 }
 
 #[repr(C)]
 pub struct SceKernelFreeMemorySizeInfo {
     /// sizeof(SceKernelFreeMemorySizeInfo)
-	pub size: i32,
+    pub size: i32,
     /// Free memory size for *_USER_RW memory
-	pub size_user: i32,
+    pub size_user: i32,
     /// Free memory size for USER_CDRAM_RW memory
-	pub size_cdram: i32,
+    pub size_cdram: i32,
     ///Free memory size for USER_MAIN_PHYCONT_*_RW memory
-	pub size_phycont: i32,
+    pub size_phycont: i32,
 }
 
 #[repr(i32)]
 pub enum SceKernelModel {
-	SCE_KERNEL_MODEL_VITA   = 0x10000,
-	SCE_KERNEL_MODEL_VITATV = 0x20000
+    SCE_KERNEL_MODEL_VITA = 0x10000,
+    SCE_KERNEL_MODEL_VITATV = 0x20000,
 }
 
 #[repr(C)]
 pub struct SceKernelMemBlockInfo {
-	pub size: SceSize,
-	pub mappedBase: *mut ::void,
-	pub mappedSize: SceSize,
-	pub memory_type: SceKernelMemoryType,
-	pub access: SceUInt32,
-	pub block_type: SceKernelMemBlockType,
+    pub size: SceSize,
+    pub mappedBase: *mut ::void,
+    pub mappedSize: SceSize,
+    pub memory_type: SceKernelMemoryType,
+    pub access: SceUInt32,
+    pub block_type: SceKernelMemBlockType,
 }
 
 #[repr(i32)]
 pub enum SceKernelMemoryAccessType {
-	SCE_KERNEL_MEMORY_ACCESS_X = 0x01,
-	SCE_KERNEL_MEMORY_ACCESS_W = 0x02,
-	SCE_KERNEL_MEMORY_ACCESS_R = 0x04
+    SCE_KERNEL_MEMORY_ACCESS_X = 0x01,
+    SCE_KERNEL_MEMORY_ACCESS_W = 0x02,
+    SCE_KERNEL_MEMORY_ACCESS_R = 0x04,
 }
 
 #[repr(i32)]
 pub enum SceKernelMemoryType {
-	SCE_KERNEL_MEMORY_TYPE_NORMAL_NC = 0x80,
-	SCE_KERNEL_MEMORY_TYPE_NORMAL    = 0xD0
+    SCE_KERNEL_MEMORY_TYPE_NORMAL_NC = 0x80,
+    SCE_KERNEL_MEMORY_TYPE_NORMAL = 0xD0,
 }
 
 #[link(kind = "static", name = "SceLibKernel_stub")]
 extern "C" {
-    pub fn sceKernelAllocMemBlock(name: *const u8, type_: SceKernelMemBlockType, size: i32, optp: *mut SceKernelAllocMemBlockOpt) -> SceUID;
+    pub fn sceKernelAllocMemBlock(
+        name: *const u8,
+        type_: SceKernelMemBlockType,
+        size: i32,
+        optp: *mut SceKernelAllocMemBlockOpt,
+    ) -> SceUID;
     pub fn sceKernelFreeMemBlock(uid: SceUID) -> i32;
     pub fn sceKernelGetMemBlockBase(uid: SceUID, basep: *mut *mut ::void) -> i32;
     pub fn sceKernelFindMemBlockByAddr(addr: *const ::void, size: SceSize) -> SceUID;
-    pub fn sceKernelGetMemBlockInfoByAddr(base: *mut ::void, info: *mut SceKernelMemBlockInfo) -> i32;
-    pub fn sceKernelGetMemBlockInfoByRange(base: *mut ::void, size: SceSize, info: *mut SceKernelMemBlockInfo) -> i32;
+    pub fn sceKernelGetMemBlockInfoByAddr(
+        base: *mut ::void,
+        info: *mut SceKernelMemBlockInfo,
+    ) -> i32;
+    pub fn sceKernelGetMemBlockInfoByRange(
+        base: *mut ::void,
+        size: SceSize,
+        info: *mut SceKernelMemBlockInfo,
+    ) -> i32;
     pub fn sceKernelAllocMemBlockForVM(name: *const u8, size: SceSize) -> SceUID;
     pub fn sceKernelSyncVMDomain(uid: SceUID, data: *const ::void, size: SceSize) -> i32;
     pub fn sceKernelOpenVMDomain() -> i32;
