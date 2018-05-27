ab3ba25c9cf02821eb72589c4ca0ab2e075a1f60
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-05-27 02:51:44 +0200

    Add all Mutex methods to `threadmgr.rs`

--- src/kernel/threadmgr.rs
+++ src/kernel/threadmgr.rs
@@ -1,6 +1,9 @@
 use ::types::SceUInt;
+use ::types::SceUInt64;
 use ::types::SceUID;
 use ::types::SceSize;
+
+type SceKernelSysClock = SceUInt64;
 
 #[repr(C)]
 pub struct SceKernelMutexOptParam {
@@ -8,10 +11,34 @@ pub struct SceKernelMutexOptParam {
     pub ceilingPriority: i32,
 }
 
+#[repr(C)]
+pub struct SceKernelMutexInfo {
+	pub size: SceSize,
+	pub mutexId: SceUID,
+	pub name: [u8; 32],
+	pub attr: SceUInt,
+	pub initCount: i32,
+	pub currentCount: i32,
+	pub currentOwnerId: SceUID,
+	pub numWaitThreads: i32,
+}
+
 #[link(kind = "static", name = "SceLibKernel_stub")]
 extern "C" {
-    pub fn sceKernelDelayThread(delay: SceUInt) -> i32;
+
+    // Mutexes
     pub fn sceKernelCreateMutex(name: *const u8, attr: SceUInt, initCount: i32, option: *mut SceKernelMutexOptParam) -> SceUID;
+    pub fn sceKernelOpenMutex(name: *const u8) -> i32;
+    pub fn sceKernelCloseMutex(mutexid: SceUID) -> i32;
+    pub fn sceKernelDeleteMutex(mutexid: SceUID) -> i32;
     pub fn sceKernelLockMutex(mutexid: SceUID, lockCount: i32, timeout: *mut u32) -> i32;
+    pub fn sceKernelLockMutexCB(mutexid: SceUID, lockCount: i32, timeout: *mut u32) -> i32;
+    pub fn sceKernelTryLockMutex(mutexid: SceUID, lockCount: i32) -> i32;
     pub fn sceKernelUnlockMutex(mutexid: SceUID, unlockCount: i32) -> i32;
+    pub fn sceKernelCancelMutex(mutexid: SceUID, lockCount: i32, numWaitThreads: *mut i32) -> i32;
+    pub fn sceKernelGetMutexInfo(mutexid: SceUID, info: *mut SceKernelMutexInfo) -> i32;
+
+    // Threads
+    pub fn sceKernelDelayThread(delay: SceUInt) -> i32;
+    
 }
