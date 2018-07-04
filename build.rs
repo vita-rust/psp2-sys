fbdef7bb6c9c8711c8f77405914853b5f1498e6e
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-07-04 18:08:21 +0200

    Allow building library without `VITASDK` after showing a warning

--- build.rs
+++ build.rs
@@ -2,7 +2,10 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let sdk = PathBuf::from(env::var("VITASDK").expect("$VITASDK is not set !"));
-    let lib = sdk.join("arm-vita-eabi").join("lib");
-    println!("cargo:rustc-link-search={}", lib.to_str().unwrap());
+    if let Ok(sdk) = env::var("VITASDK").map(PathBuf::from) {
+        let lib = sdk.join("arm-vita-eabi").join("lib");
+        println!("cargo:rustc-link-search={}", lib.to_str().unwrap());
+    } else {
+        println!("cargo:warning=$VITASDK not set!");
+    }
 }
